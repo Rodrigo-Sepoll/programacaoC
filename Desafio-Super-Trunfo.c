3b6699101113977a34c58afaec87ffffb1175c1e
Rodrigo-Sepoll <rodrigo.sepoll@gmail.com>
2025-09-18 00:55:49 +0000

    Declaração de variáveis para a 1ª Carta

--- Desafio-Super-Trunfo.c
+++ Desafio-Super-Trunfo.c
@@ -3,3 +3,14 @@
 int main() {
     
     printf("Desafio Super Trunfo C - Nivel Aventureiro!\n\n");
+
+    // Declaração de variáveis para a Carta 1
+    char Estado1[2]; // Informe o Estado (A-H): 
+    char Codigo_da_Carta1[4]; // Ex: A01, B02 (3 caracteres)
+    char Nome_da_Cidade1[20]; // Tamanho suficiente para o nome da cidade
+    int Populacao1;
+    float Area1;
+    float PIB1;
+    int Pontos_Turisticos1;
+    float Densidade_Populacional1;
+    float PIB_per_capita1;
